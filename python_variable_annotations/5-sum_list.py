5fd91c510d984ade15009f46d58e61ed6bcd55a7
Gbc6292 <6292@holbertonstudents.com>
2023-08-28 16:19:41 -0400

    correcting method

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,5 +5,4 @@ as argument and returns the string representation of the float"""
 
 def sum_list(input_list: list[float]) -> float:
     """Return the result of a list sum"""
-    a = sum(input_list)
     return sum(input_list)
