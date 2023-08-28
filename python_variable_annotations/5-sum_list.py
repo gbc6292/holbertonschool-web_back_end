b61a04369bc2cfe3ccc343a2d9984b9b1f9127d9
Gbc6292 <6292@holbertonstudents.com>
2023-08-28 16:10:14 -0400

    Adding sum list method

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_str that takes a float n
+as argument and returns the string representation of the float"""
+
+
+def sum_list(input_list: float) -> float:
+    """Return the result of a list sum"""
+    return sum(input_list)
