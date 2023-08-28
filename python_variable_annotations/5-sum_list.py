4230adbb5d95c7f558bfae099818882dc61a383e
Gbc6292 <6292@holbertonstudents.com>
2023-08-28 16:17:27 -0400

    Correting variable type

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,6 +3,7 @@
 as argument and returns the string representation of the float"""
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: list[float]) -> float:
     """Return the result of a list sum"""
+    a = sum(input_list)
     return sum(input_list)
