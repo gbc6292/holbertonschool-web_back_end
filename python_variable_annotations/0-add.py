082699ba542c26aad25a76627f5c37c8c2d3ad06
Gbc6292 <6292@holbertonstudents.com>
2023-08-28 14:01:16 -0400

    Adding float sum

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that take two floats and return their sum as a float"""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b"""
+    return a + b
