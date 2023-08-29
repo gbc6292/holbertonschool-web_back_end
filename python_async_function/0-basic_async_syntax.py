74784fb55a03f96dc3eb2e9a5050ff1e3985413a
Gbc6292 <6292@holbertonstudents.com>
2023-08-29 13:58:00 -0400

    correcting typos

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,6 +2,7 @@
 """asynchronous coroutine that takes in an integer argument (max_delay,
 with a default value of 10) named wait_random that waits for a random delay between 0
 and max_delay (included and float value) seconds and eventually returns i"""
+
 import asyncio
 import random
 
