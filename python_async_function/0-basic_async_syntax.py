ee1962d9a34c0bfb83ba1389300f22364ac503e5
Gbc6292 <6292@holbertonstudents.com>
2023-09-01 18:20:08 -0400

    Correcting documentation

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,8 +8,15 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Wait a random delay between max and 0 and return
-    the random delay in a float datatype"""
+    """
+    Asynchronous coroutine that generates a random delay between 0 and max_delay seconds (inclusive).
+
+    Args:
+        max_delay (int, optional): The maximum delay in seconds. Defaults to 10.
+
+    Returns:
+        float: A random delay in seconds as a floating-point number.
+    """
     random_delay = random.uniform(0, max_delay)
 
     await asyncio.sleep(random_delay)
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,16 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous function that generates a list of random delays.
+
+    Args:
+        n (int): The number of random delays to generate.
+        max_delay (int): The maximum delay in seconds for each random delay.
+
+    Returns:
+        List[float]: A list of random delays as floating-point numbers.
+    """
     delay_list: List[float] = []
     task: float = []
 
