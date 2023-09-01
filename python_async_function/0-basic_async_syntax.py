e339b89d2d2cb8d71df78488e2aec98435320ef6
Gbc6292 <6292@holbertonstudents.com>
2023-09-01 18:26:08 -0400

    Correcting output

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""asynchronous coroutine that takes in an integer argument (max_delay,
-with a default value of 10) named wait_random that waits for a random delay between 0
-and max_delay (included and float value) seconds and eventually returns i"""
+"""asynchronous coroutine that takes in an integer argument (max_delay
+with a default value of 10) named wait_random that waits for a random
+delay between 0 and max_delay (included and float value) seconds and
+eventually returns i"""
 
 import asyncio
 import random
@@ -9,10 +10,12 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     """
-    Asynchronous coroutine that generates a random delay between 0 and max_delay seconds (inclusive).
+    Asynchronous coroutine that generates a random delay between
+    0 and max_delay seconds (inclusive).
 
     Args:
-        max_delay (int, optional): The maximum delay in seconds. Defaults to 10.
+        max_delay (int, optional): The maximum delay in seconds.
+        Defaults to 10.
 
     Returns:
         float: A random delay in seconds as a floating-point number.
