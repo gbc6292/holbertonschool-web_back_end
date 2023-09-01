11cc3b009ad1f8196434169c97abc074a27b3563
Gbc6292 <6292@holbertonstudents.com>
2023-09-01 18:35:25 -0400

    Adding task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+from typing import List
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the average execution time of wait_n(n, max_delay).
+
+    Args:
+        n (int): The number of random delays to generate.
+        max_delay (int): The maximum delay in seconds for each random delay.
+
+    Returns:
+        float: The average execution time in seconds.
+    """
+    start_time = time.time()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    return total_time / n
+
+
+if __name__ == "__main__":
+    n = 5
+    max_delay = 9
+
+    avg_execution_time = measure_time(n, max_delay)
+    print(avg_execution_time)
