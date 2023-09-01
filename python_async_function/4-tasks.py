3fec801b1fbe0c9ab5e9d4cd1822d1d1c9099822
Gbc6292 <6292@holbertonstudents.com>
2023-09-01 18:59:35 -0400

    correcting output

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,37 +4,20 @@ wait_n except task_wait_random is being called."""
 import asyncio
 from typing import List
 
-# Import the task_wait_random function from the same module.
-task_wait_random = __import__('3-tasks import').task_wait_random
+
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Asynchronous function that generates a list of random delays using tasks.
-
-    Args:
-        n (int): The number of random delays to generate.
-        max_delay (int): The maximum delay in seconds for each random delay.
-
-    Returns:
-        List[float]: A list of random delays as floating-point numbers.
-    """
+    """Return a list of float ascending order"""
     delay_list: List[float] = []
-    tasks: List[asyncio.Task] = []
+    tasks: List = []
 
     for _ in range(n):
-        task = task_wait_random(max_delay)
-        tasks.append(task)
+        tasks.append(task_wait_random(max_delay))
 
-    for completed_task in asyncio.as_completed(tasks):
-        delay = await completed_task
+    for task in asyncio.as_completed((tasks)):
+        delay = await task
         delay_list.append(delay)
 
     return delay_list
-
-if __name__ == "__main__":
-    n = 5
-    max_delay = 6
-
-    result = asyncio.run(task_wait_n(n, max_delay))
-    print(result)
