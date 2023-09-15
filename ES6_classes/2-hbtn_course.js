9d3fdce89c293a176dbc5b747caeb70f782f3b7c
Gbc6292 <6292@holbertonstudents.com>
2023-09-15 17:58:44 -0400

    Adding ES6Classes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,68 +1,42 @@
-// Implementing a class named HolbertonCourse
-
-class HolbertonCourse {
+/* eslint-disable */
+export default class HolbertonCourse {
     constructor(name, length, students) {
-        // Verifying the types of attributes during objects creation
-        if (typeof name == 'string') {
-            this._name = name;
-        } else {
-            throw new TypeError('Name must be a string');
+        if (typeof name !== 'string') throw TypeError('name must be a string');
+        if (typeof length !== 'number') throw TypeError('length must be a number');
+        if (students.constructor !== Array && students.every((el) => typeof el === 'string')) {
+            throw TypeError('students must be an array of strings');
         }
-
-        if (typeof length == 'number') {
-            this._length = length;
-        } else {
-            throw new TypeError('Lenght must be a number');
-        }
-
-        if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
-            this._students = students;
-        } else {
-            throw new TypeError('Student must be an array of strings');
-        }
+        this._name = name;
+        this._length = length;
+        this._students = students;
     }
 
-    //Getter for name
     get name() {
         return this._name;
     }
 
-    //Setter for name
-    set name(newName) {
-        if (typeof newName === 'string') {
-            this._name = newName;
-        } else {
-            throw new TypeError('Name must be a string');
-        }
-    }
-
-    //Getter for length
     get length() {
         return this._length;
     }
 
-    //Setter for length
-    set length(newLength) {
-        if (typeof newLength === 'number') {
-            this._length = newLength;
-        } else {
-            throw new TypeError('Length must be a number');
-        }
-    }
-
-    //Getter for students
-    get student() {
+    get students() {
         return this._students;
     }
 
-    //Setter for students
+    set name(newName) {
+        if (typeof newName !== 'string') throw TypeError('name must be a string');
+        this._name = newName;
+    }
+
+    set length(newLength) {
+        if (typeof newLength !== 'number') throw TypeError('length must be a number');
+        this._length = newLength;
+    }
+
     set students(newStudents) {
-        if (Array.isArray(newStudents) && newStudents.every((student) => typeof student === 'string')) {
-            this._students = newStudents;
-        } else {
-            throw new TypeError('Student must be an array of strings');
+        if (newStudents.constructor !== Array && newStudents.every((el) => typeof el === 'string')) {
+            throw TypeError('students must be an array of strings');
         }
+        this._students = newStudents;
     }
 }
-
-export default HolbertonCourse;
--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,51 +1,27 @@
-// Implementing class named Currency
-
-class Currency {
+/* eslint-disable */
+export default class Currency {
     constructor(code, name) {
-        if (typeof code === 'string') {
-            this._code = code;
-        } else {
-            throw new TypeError('Code must be a string')
-        }
-
-        if (typeof name === 'string') {
-            this._name = name;
-        } else {
-            throw new TypeError('Name must be a string');
-        }
+        this._code = code;
+        this._name = name;
     }
 
-    //Getter for code
     get code() {
         return this._code;
     }
 
-    //Setter for code
-    set code(newCode) {
-        if (typeof newCode === 'string') {
-            this._code = newCode;
-        } else {
-            throw new TypeError('Code must be a string');
-        }
-    }
-
-    //Getter for name
     get name() {
         return this._name;
     }
 
-    //Setter for name
+    set code(newCode) {
+        this._code = newCode;
+    }
+
     set name(newName) {
-        if (typeof newName === 'string') {
-            this._name = newName;
-        } else {
-            throw new TypeError('Name must be a string');
-        }
+        this._name = newName;
     }
 
     displayFullCurrency() {
-        return '${ this._name } (${ this._code })';
+        return `${this._name} (${this._code})`;
     }
 }
-
-export default Currency
